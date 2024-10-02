9b3123d1203f02a99349ce1d0b13bfef77845afd
David <166518766+davidcuadrado@users.noreply.github.com>
2024-10-02 13:48:28 +0200

    FruitView update

--- S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/services/FruitView.java
+++ S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/services/FruitView.java
@@ -1,11 +1,13 @@
 package cat.itacademy.s04.t02.n01.services;
 
+import cat.itacademy.s04.t02.n01.model.Fruit;
+
 public class FruitView {
 	
-	public void printFruitDetails(String studentName, String studentRollNo) {
-        System.out.println("Student:");
-        System.out.println("Name: " + studentName);
-        System.out.println("Roll No: " + studentRollNo);
+	public void printFruitDetails(Fruit fruit) {
+        System.out.println("Fuit (ID:" + fruit.getId());
+        System.out.println("Name: " + fruit.getName());
+        System.out.println("Quantity (kilo): " + fruit.getKiloQuantity());
     }
 
 
