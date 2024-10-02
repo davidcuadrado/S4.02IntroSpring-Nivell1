452fb3e9451cbcc03ccdb85a827a20636b531674
David <166518766+davidcuadrado@users.noreply.github.com>
2024-10-02 23:42:22 +0200

    Model Fruit update

--- S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/model/Fruit.java
+++ S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/model/Fruit.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "fruit")
 public class Fruit {
 
 	@Id
@@ -18,10 +20,6 @@ public class Fruit {
 	
 	public int getId() {
 		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 
 	public String getName() {
