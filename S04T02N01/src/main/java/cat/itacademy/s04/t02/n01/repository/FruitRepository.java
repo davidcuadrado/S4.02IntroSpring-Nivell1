fe5a51c1c47d4ab3bd935d7c6e0690844824c33d
David <166518766+davidcuadrado@users.noreply.github.com>
2024-10-03 00:30:14 +0200

    Repository update
    feat. FruitRepository extends JpaRepository

--- S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/repository/FruitRepository.java
+++ S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/repository/FruitRepository.java
@@ -1,5 +1,10 @@
 package cat.itacademy.s04.t02.n01.repository;
 
-public interface FruitRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import cat.itacademy.s04.t02.n01.model.Fruit;
+
+@Repository
+public interface FruitRepository extends JpaRepository<Fruit, Integer> {
 
 }
